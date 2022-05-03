4e60b34e8ff8a851c60644f54b2c68b26da19e7f
javier <francisco.rodriguez@magnothecnology.com>
2022-05-03 01:37:51 -0500

    Agregando test para getInfo

--- semana3/spiderverse/test/spiderman.test.js
+++ semana3/spiderverse/test/spiderman.test.js
@@ -10,4 +10,9 @@ describe("Unit test for Spiderman class", () => {
       expect(andrewGarfield.movies).toBe(2)
       expect(andrewGarfield.studio).toBe("Sony")
     });
+
+    test('2) Usando el metodo getInfo()', () => {
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+    })
   })

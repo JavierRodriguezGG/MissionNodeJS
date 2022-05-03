d40aacb8c584e48ece7aeede543fcc8c2fc34dd9
javier <francisco.rodriguez@magnothecnology.com>
2022-05-02 19:20:49 -0500

    Fallando la prueba intencionalmente

--- semana3/my_launchx_app/test/missionCommander.test.js
+++ semana3/my_launchx_app/test/missionCommander.test.js
@@ -1,7 +1,7 @@
 const MissionCommander = require('../app/missionCommander');
 describe("Prueba de unidad MissionCommander class", () => {
     test('1) Create a mission commander objet', () => {
-      const myMissionCommander =  new MissionCommander("Woopa")
+      const myMissionCommander =  new MissionCommander("Woopa!!!")
       expect(myMissionCommander.name).toBe("Woopa");
     });
   })

6a23674b0ac69bbe9cec3e5ba9d4638e3a4b8ee2
javier <francisco.rodriguez@magnothecnology.com>
2022-05-03 19:45:30 -0500

    Evitando mandar dateCreated y lastUpdate

--- semana3/twitter/test/models/User.test.js
+++ semana3/twitter/test/models/User.test.js
@@ -2,14 +2,14 @@ const User = require('./../../app/models/user')
 
 describe("Unit tests for User class", () => {
     test('Create an User object', () => {
-        const user = new User(1, "javierrgg", "Javi", "Bio", "datecreated", "lastupdate")
+        const user = new User(1, "javierrgg", "Javi", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("javierrgg")
         expect(user.name).toBe("Javi")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("datecreated")
-        expect(user.lastUpdated).toBe("lastupdate")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 
 })

f5982bca03ccc9d14505b3adf2cafad7167b0b8a
javier <francisco.rodriguez@magnothecnology.com>
2022-05-09 20:13:07 -0500

    Segunda prueba, funcionando

--- semana3/twitter/test/services/userServices.test.js
+++ semana3/twitter/test/services/userServices.test.js
@@ -1,9 +1,9 @@
-const userService = require('./../../app/services/userServices')
+const UserService = require('./../../app/services/userServices')
 
 describe("Test for userServices", () => {
 
     test("1) Create a new user using the userService", () => {
-        const user = userService.create(1, "javierrogg", "Javier")
+        const user = UserService.create(1, "javierrogg", "Javier")
         expect(user.username).toBe("javierrogg")
         expect(user.name).toBe("Javier")
         expect(user.id).toBe(1)
